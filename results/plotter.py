501ac1815a94eb17e045c693fdb9e801ac7d6656
Stephen Tu <stephentu@csail.mit.edu>
2013-02-26 16:09:13 -0800

    new benchmark results

--- results/plotter.py
+++ results/plotter.py
@@ -3,6 +3,7 @@
 import matplotlib
 import pylab as plt
 import sys
+import math
 
 BENCHMARKS=('readonly', 'queue')
 POLICIES = ('global_lock', 'per_node_lock', 'lock_free', 'lock_free_rcu')
@@ -13,13 +14,16 @@ if __name__ == '__main__':
   for bench in BENCHMARKS:
     fig = plt.figure()
     ax = plt.subplot(111)
+    #logscale = bench == 'readonly'
+    logscale = False
+    trfm = (lambda x: math.log(x, 10)) if logscale else (lambda x: x)
     for policy in POLICIES:
       configs = [(x, y) for (x, y) in RESULTS if x['bench'] == bench and x['policy'] == policy]
       configs = sorted([(x['threads'], y) for (x, y) in configs], key=lambda x: x[0])
-      ax.plot([x[0] for x in configs], [x[1] for x in configs])
+      ax.plot([x[0] for x in configs], [trfm(x[1]/float(x[0])) for x in configs])
 
-    ax.set_xlabel('nthreads')
-    ax.set_ylabel('throughput (ops/sec)')
+    ax.set_xlabel('num cores')
+    ax.set_ylabel('throughput (ops/sec/core)')
     ax.set_title(bench)
 
     # hacky: see http://stackoverflow.com/questions/4700614/how-to-put-the-legend-out-of-the-plot
